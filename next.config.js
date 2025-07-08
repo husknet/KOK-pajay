4ed4c8430e1cf7f8c3a0a126d7f903d6c29cca88
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-08 19:55:57 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,14 @@
- /** @type {import('next').NextConfig} */
-- const nextConfig = {
--   reactStrictMode: true,
--   swcMinify: true,
--   experimental: {
--     appDir: true,
--   },
--   images: {
--     domains: ['api.telegram.org'],
--   },
--   env: {
--     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
--     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
--   },
-- }
-+ const nextConfig = {
-+   reactStrictMode: true,
-+   swcMinify: true,
-+   images: {
-+     domains: ['api.telegram.org'],
-+   },
-+   env: {
-+     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-+     TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
-+   },
-+ }
- 
- module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['api.telegram.org'],
+  },
+  env: {
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+  },
+}
+
+module.exports = nextConfig
